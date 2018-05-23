ff8cf423f84af479d2742c674687da22580655cd
James Sloat <james.sloat@lsrlaw.com>
2018-05-23 16:21:58 -0400

    added failing test for login error message showing

--- test/login.test.js
+++ test/login.test.js
@@ -4,9 +4,10 @@ import {mount} from 'vue-test-utils'
 describe('Login Component Tests', () => {
 
   let component;
+  let $api;
 
   beforeEach(() => {
-    const $api = {
+    $api = {
       login: jest.fn()
     };
 
@@ -23,17 +24,41 @@ describe('Login Component Tests', () => {
 
   test('clicking the log-in button shows the loading spinner', () => {
     expect(component.vm.showingLoadingSpinner).toBe(false);
-    const button = component.find('button');
-    button.trigger('click');
+    attemptLogin();
     expect(component.vm.showingLoadingSpinner).toBe(true);
   });
 
   test('logging in attempts to call login service', () => {
-    // getReturnsAPromise();
-    const button = component.find('button');
-    button.trigger('click');
+    attemptLogin();
 
     expect(component.vm.$api.login).toBeCalled();
   });
+
+  test('login passes the right username and password to the service', () => {
+    const username = component.find('input[type="text"]');
+    const password = component.find('input[type="password"]');
+
+    component.vm.username = 'myusername';
+    component.vm.password = 'mypassword';
+
+    attemptLogin();
+
+    expect(component.vm.$api.login).toHaveBeenCalledWith('myusername', 'mypassword');
+  });
+
+  test('failure to log-in displays login-failure message', () => {
+    $api.login = jest.fn(() => Promise.reject({data: true}))
+    loginErrorMessageIsShowing();
+  });
+
+  function loginErrorMessageIsShowing() {
+    let error = component.find('p.error');
+    expect(error.exists()).toBe(true);
+  }
+
+  function attemptLogin() {
+    const button = component.find('button');
+    button.trigger('click');
+  }
 
 });
